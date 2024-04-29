d9faebe36383fcfbb7ea436795cdb46cc32dd82a
SzilagyiBotond <ancelotti156@gmail.com>
2024-04-30 00:13:52 +0300

    Now good

--- obj/Debug/net8.0-windows7.0/Team42Test.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows7.0/Team42Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Team42Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aec6aae68f15c9329b06269ba25a9f918ab61680")]
 [assembly: System.Reflection.AssemblyProductAttribute("Team42Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Team42Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
